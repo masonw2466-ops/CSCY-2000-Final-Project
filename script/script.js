6b225885772f9c14c87ea78a6ba5f6a5a6610a19
masonw2466-ops <masonw2466@gmail.com>
2025-11-18 11:53:22 -0700

    Added framework and JS that adds links

---
+++ script/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("navigation-placeholder").innerHTML = `
+    <nav>
+		<ul>
+      		<li><a href="index.html">Home</a></li>
+      		<li><a href="page1.html">Page1</a></li>
+      		<li><a href="page2.html">Page2</a></li>
+      		<li><a href="page3.html">Page3</a></li>
+      		<li><a href="page4.html">Page4</a></li>
+		</ul>
+    </nav>
+  `;
+});
